422c8fbb8642f674f208b7f65cb2cb1df10b47f1
Luke Del Giudice <wcc5ub@virginia.edu>
2025-01-24 10:53:25 -0500

    fix naming drive

--- drive.R
+++
@@ -1,8 +0,0 @@
-
-drive <- function(down, ytg, fp) {
-  new_down = down
-  new_ytg = ytg
-  new_fp = fp
-  return(new_down, new_ytg, new_fp)
-}
-  
---
+++ run_drive.R
@@ -0,0 +1,5 @@
+
+drive <- function(yd_line) {
+  new_yd_line = yd_line + rpois(1, 50)
+  return(newyd_line)
+}
