40762b42458c5f49569db108e622c747ba8bf0d9
Luke Del Giudice <wcc5ub@virginia.edu>
2025-01-31 10:28:21 -0500

    Update create_transition.R

--- Lab 3/create_transition.R
+++ Lab 3/create_transition.R
@@ -1,13 +1,14 @@
 
-construct <- function(file_path, years) {
+construct <- function(df, years) {
   teams <- 4 # fix this to parse for teams
   transition <- matrix(0, nrow = teams, ncol = teams)
   
+  games <- readRDS("C:/Users/luked/Repositories/TeamStarbucks/data.rds")
   
   
-  
-  for (game in 1:games) {
+  for (i in 1:nrow(games)) {
     # get team 1 and team 2
+    games[i]
     
     if (p1 > p2) {
       transition[t1][t2] = transition[t1][t2] + 1
