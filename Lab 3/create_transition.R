47f43361d8d8fabcb8576bd2d6dffb27befdb559
Luke Del Giudice <wcc5ub@virginia.edu>
2025-01-31 10:21:56 -0500

    blocking out create_transition solution

---
+++ Lab 3/create_transition.R
@@ -0,0 +1,19 @@
+
+construct <- function(file_path, years) {
+  teams <- 4 # fix this to parse for teams
+  transition <- matrix(0, nrow = teams, ncol = teams)
+  
+  
+  
+  
+  for (game in 1:games) {
+    # get team 1 and team 2
+    
+    if (p1 > p2) {
+      transition[t1][t2] = transition[t1][t2] + 1
+    }
+    
+  }
+  
+  # normalize
+}
