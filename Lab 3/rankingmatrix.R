bff1d4e0b261628eb2f9a95ed27f8f3f3204e6f9
Patrick Means <158609912+Spagh3tt0@users.noreply.github.com>
2025-02-06 20:23:15 -0500

    Update rankingmatrix.R

---
+++ Lab 3/rankingmatrix.R
@@ -0,0 +1,15 @@
+transition_matrix <- function(game_data) {
+  teams <- unique(c(game_data$Visiting_Team, game_data$Home_Team))
+  n <- length(teams)
+  loss_matrix <- matrix(0, nrow = n, ncol = n, dimnames = list(teams, teams))
+  
+  for (i in 1:nrow(game_data)) {
+    if (gamedata$Visiting_Score >= game_data$Home_Score) {
+      loss_matrix[game_data$Home_Team[i], game_data$Visiting_Team[i]] <- 
+        loss_matrix[game_data$Home_Team[i], game_data$Visiting_Team[i]] + 1
+    } else {
+      loss_matrix[game_data$Visiting_Team[i], game_data$Home_Team[i]] <- 
+        loss_matrix[game_data$Visiting_Team[i], game_data$Home_Team[i]] + 1
+    }
+  }
+}
