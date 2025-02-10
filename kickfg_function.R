6b64969f4498a360de3c1dc68441f45280c31ed5
Patrick Means <158609912+Spagh3tt0@users.noreply.github.com>
2025-02-10 10:50:18 -0500

    Update kickfg_function.R
    Just added some more notes on the function for future reference. Help detail what to add to make it more complex/accurate

--- kickfg_function.R
+++ kickfg_function.R
@@ -1,4 +1,4 @@
-#Accounts for most rigid scenario but should be expanded to consider factors such as time left, point differential, and range
+#Accounts for most rigid scenario but should be expanded to consider factors such as time left, point differential, and range while potentially ignoring ytg
 
 kickfg <- function(down, ytg, fp) {
   if (down == 4 && ytg < 2 && fp <= 95 && fp >= 70) {
@@ -8,4 +8,5 @@ kickfg <- function(down, ytg, fp) {
     return(FALSE)  
   }
 
+#Find percentages at what rate field goals are blocked/missed vs percentage of conversions of low ytg 4th down conversions to find when its worth to go for what
 
