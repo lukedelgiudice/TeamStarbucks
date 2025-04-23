9078500b9b7ecf31636d630404cdad1a987e70f9
Luke Del Giudice <luke.delgiudice@outlook.com>
2025-04-23 02:36:19 -0400

    👽

--- layer_yards_gained.R
+++ layer_yards_gained.R
@@ -1,27 +1,37 @@
-sample_yards_gained <- function(play_call, player_position, pass_length, red_zone, unexpected) {
-  if (unexpected) {
-    distributions <- readRDS("yards_distributions_unexpected.rds")
+sample_yards_gained <- (function() {
+  distributions_expected <- NULL
+  distributions_unexpected <- NULL
+  
+  function(play_call, player_position, pass_length, red_zone, unexpected) {
+    if (is.null(distributions_expected)) {
+      distributions_expected <<- readRDS("yards_distributions.rds")
+    }
+    if (is.null(distributions_unexpected)) {
+      distributions_unexpected <<- readRDS("yards_distributions_unexpected.rds")
+    }
+    
+    distributions <- if (unexpected) {
+      distributions_unexpected
+    }
+    else {
+      distributions_expected
+    }
+    
+    key <- if (play_call == "pass") {
+      paste(play_call, player_position, pass_length, red_zone, unexpected, sep = "_")
+    }
+    else {
+      paste(play_call, player_position, NA, red_zone, unexpected, sep = "_")
+    }
+    
+    dist <- distributions[[key]]
+    
+    if (is.null(dist)) {
+      return(round(rnorm(1, mean = 5, sd = 5)))
+    }
+    
+    component <- sample(seq_along(dist$lambda), 1, prob = dist$lambda)
+    yards <- rnorm(1, mean = dist$mu[component], sd = dist$sigma[component])
+    return(round(max(0, yards)))
   }
-  
-  else {
-    distributions <- readRDS("yards_distributions.rds")
-  }
-  
-  key <- if (play_call == "pass") {
-    paste(play_call, player_position, pass_length, red_zone, unexpected, sep = "_")
-  }
-  
-  else {
-    paste(play_call, player_position, NA, red_zone, unexpected, sep = "_")
-  }
-  
-  dist <- distributions[[key]]
-  
-  if (is.null(dist)) {
-    return(round(rnorm(1, mean = 5, sd = 5)))
-  }
-  
-  component <- sample(seq_along(dist$lambda), 1, prob = dist$lambda)
-  yards <- rnorm(1, mean = dist$mu[component], sd = dist$sigma[component])
-  return(round(max(0, yards)))
-}
+})()
--- predict_field_goal.R
+++ predict_field_goal.R
@@ -1,13 +1,18 @@
-footballData <- readRDS("pbp2014-2024.rds")
-fieldGoalData <- footballData[!is.na(footballData$play_type) & footballData$play_type == "field_goal", ]
-fieldGoalData$field_goal_result <- factor(fieldGoalData$field_goal_result, levels = c("missed", "made"))
-logistic_model <- glm(field_goal_result ~ yardline_100, data = fieldGoalData, family = "binomial")
-
-predict_field_goal <- function(yardline) {
-  if (yardline < 0 || yardline > 100) {
-    stop("yardline must be between 0 and 100")
-  }
+predict_field_goal <- (function() {
+  footballData <- readRDS("pbp2014-2024.rds")
   
-  predicted_prob <- predict(logistic_model, newdata = data.frame(yardline_100 = yardline), type = "response")
-  return(predicted_prob)
-}
+  fieldGoalData <- footballData[!is.na(footballData$play_type) & footballData$play_type == "field_goal", ]
+  
+  fieldGoalData$field_goal_result <- factor(fieldGoalData$field_goal_result, levels = c("missed", "made"))
+  
+  logistic_model <- glm(field_goal_result ~ yardline_100, data = fieldGoalData, family = "binomial")
+  
+  function(yardline) {
+    if (yardline < 0 || yardline > 100) {
+      stop("yardline must be between 0 and 100")
+    }
+    
+    predicted_prob <- predict(logistic_model, newdata = data.frame(yardline_100 = yardline), type = "response")
+    return(predicted_prob)
+  }
+})()
